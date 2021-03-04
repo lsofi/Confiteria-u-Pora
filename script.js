4be10bb0c5e35e688977a7b7603da333288da159
lsofi <sjuarezvillagra@gmail.com>
2021-03-04 20:04:53 -0300

    error arreglado y prueba final responsive

--- script.js
+++ script.js
@@ -2,18 +2,10 @@ $(window).scroll(function(){
     var scroll = $(window).scrollTop();
     if(scroll >= 350){
         document.querySelector(".navbar").style.backgroundColor="rgb(218, 218, 65)";
+        document.querySelector(".navbar").style.heigth="80px";
+        document.querySelector(".navbar").style.paddingTop="1.5rem";
     }
     else{
         document.querySelector(".navbar").style.backgroundColor="transparent";
     }
-});
-$(".nav-item").click(function(){
-        var target = $(this.hash);
-        target = target.length ? target : $('(name=' + this.hash.slice(1) + ')');
-        if(target.length){
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 3000);
-            return false
-        }
 });
